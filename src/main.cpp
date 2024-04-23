65765c482ae33ac47f39c99e9c537dc5487faac5
Santeri Rytkönen <rytkonen.santeri123@gmail.com>
2024-04-23 06:35:16 +0300

    Remove includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,10 @@
-#include "app_edit.h"
 #include <QApplication>
-#include <QComboBox>
-#include <QVBoxLayout>
-#include <QWidget>
-#include <QPushButton>
-#include <QTextEdit>
-#include <QFileDialog>
-#include <QDir>
-#include <QLabel>
-#include <QIcon>
-#include <QProcess>
-#include <QFile>
-#include <QDebug>
-#include <QDialog>
 #include "main_window.h"
 #include "utils.h"
-
-
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
     createMyAppDirectory();
-
     MainWindow mainWindow;
     mainWindow.show();
     return app.exec();
